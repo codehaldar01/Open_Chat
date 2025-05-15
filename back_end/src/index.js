fb96341dc91c7afaff64ee0b3ba71de305f6160b
codehaldar01 <codehaldar@gmail.com>
2025-05-15 23:07:18 +0530

    debugged an error

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -30,7 +30,7 @@ app.use("/api/msg/",msgRoutes);
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../front_end/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../front_end/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../front_end/build', "dist", 'index.html'));
     //apart from api/auth and api/msg all the requests will be handled by this
     //this will show the index.html file of the front_end/build folder
   });
